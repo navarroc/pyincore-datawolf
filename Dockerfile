17dd962d8fbac6454f7bf4c1674ace849569bf62
Chris Navarro <cmnavarr@illinois.edu>
2025-07-15 15:32:13 -0500

    Updated to handle some pyincore bugs that are not yet fixed

--- Dockerfile
+++ Dockerfile
@@ -5,25 +5,25 @@ RUN  apk add python3 && \
 
 # install miniconda and pyincore
 RUN apk --update add curl wget ca-certificates libstdc++ glib && \
-    wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-node-bower/master/sgerrand.rsa.pub && \
-    curl -L "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.23-r3/glibc-2.23-r3.apk" -o glibc.apk && \
+    wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub && \
+    curl -L "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.28-r0/glibc-2.28-r0.apk" -o glibc.apk && \
     apk add glibc.apk && \
-    curl -L "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.23-r3/glibc-bin-2.23-r3.apk" -o glibc-bin.apk && \
+    curl -L "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.28-r0/glibc-bin-2.28-r0.apk" -o glibc-bin.apk && \
     apk add glibc-bin.apk && \
-    curl -L "https://github.com/andyshinn/alpine-pkg-glibc/releases/download/2.25-r0/glibc-i18n-2.25-r0.apk" -o glibc-i18n.apk && \
+    curl -L "https://github.com/andyshinn/alpine-pkg-glibc/releases/download/2.28-r0/glibc-i18n-2.28-r0.apk" -o glibc-i18n.apk && \
     apk add --allow-untrusted glibc-i18n.apk && \
     /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8 && \
     /usr/glibc-compat/sbin/ldconfig /lib /usr/glibc/usr/lib && \
     rm -rf glibc*apk /var/cache/apk/* && \
-    wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.8.3-Linux-x86_64.sh -O miniconda.sh && \
+    wget https://repo.anaconda.com/miniconda/Miniconda3-py39_23.11.0-2-Linux-x86_64.sh -O miniconda.sh && \
     sh miniconda.sh -b
 
 ENV PATH="/root/miniconda3/condabin:$PATH"
 # install pyincore
 WORKDIR /app
-COPY run_pyincore.sh dw_pyincore.py pyincore-env.yaml input_definition.json /app
-RUN conda env create -f /app/pyincore-env.yaml && \
+COPY run_pyincore_studio.sh dw_pyincore_studio.py pyincore-env.yaml /app
+RUN conda env create -f /app/pyincore-env.yaml --solver=libmamba && \
   conda clean -a -y
 
 # Entrypoint - this will activate conda and run dw_pyincore.py with given arguments
-ENTRYPOINT ["/app/run_pyincore.sh"]
+ENTRYPOINT ["/app/run_pyincore_studio.sh"]
--- dw_pyincore_studio.py
+++ dw_pyincore_studio.py
@@ -4,19 +4,26 @@
 """
 import importlib
 import os
-from pyincore import IncoreClient, FragilityService, MappingSet, Dataset
+from pyincore import IncoreClient, FragilityService, MappingSet, Dataset, NetworkDataset
 import argparse
 import datetime
+import glob
+import geopandas as gpd
 
+# These are to add support for the different network dataset types
+network_dataset_map = {
+    "incore:epnNetwork": ["ergo:powerLineTopo", "incore:epf", "incore:networkGraph"],
+    "incore:waterNetwork": ["ergo:buriedPipelineTopology", "ergo:waterFacilityTopo", "incore:networkGraph"]
+}
 
 def main(cl_args):
     print("start time:-", datetime.datetime.now())
 
-    # For local testing, add path to the token
-    # token = /path/to/token
+    #token = "/root/.incore/.a0309ff368531845cbeef9a617fcf11361da600ec8cc1467b2a579cb52890f85_token"
 
     # IN-CORE user
     incore_user = os.environ['DATAWOLF_USER']
+    #incore_user = args.user
     print("user in environment")
     print(incore_user)
 
@@ -29,8 +36,9 @@ def main(cl_args):
         "username": incore_user,
         "usergroups": ["incore_user"],
     }
-    # Create IN-CORE client, use the first client if testing locally
-    # client = IncoreClient(service_url, token)
+    # Create IN-CORE client
+
+    #client = IncoreClient(service_url, token)
     client = IncoreClient(**user)
 
     # pyIncore analysis in the form of module path and analysis class to load
@@ -75,6 +83,11 @@ def main(cl_args):
         old_filename = result_name + ".csv"
         new_filename = result_name + "_max_state.csv"
         os.rename(old_filename, new_filename)
+    if analysis_name == "ResidentialBuildingRecovery":
+        result_name = args_dict["result_name"]
+        old_filename = result_name + "_recovery.csv"
+        new_filename = result_name + "_recovery_time.csv"
+        os.rename(old_filename, new_filename)
 
 
 def set_analysis_inputs(analysis, fragility_service, args):
@@ -93,9 +106,15 @@ def set_analysis_inputs(analysis, fragility_service, args):
             elif param_type == float and len(param_arg) > 0:
                 param_arg = float(param_arg)
             elif param_type == bool and len(param_arg) > 0:
-                param_arg = bool(param_arg)
+                param_arg = parse_bool(param_arg)
             if param_arg is not None and len(str(param_arg)) > 0:
                 analysis.set_parameter(param_id, param_arg)
+
+    # Temp fix for retrofit strategy pyincore bug
+    update_dataset_id = False
+    if args.__contains__("retrofit_strategy") and args["retrofit_strategy"] is not None:
+        print("found retrofit strategy")
+        update_dataset_id = True
 
     # Load the input datasets and any fragility mapping
     input_datasets = spec["input_datasets"]
@@ -104,26 +123,37 @@ def set_analysis_inputs(analysis, fragility_service, args):
         input_types = input_dataset["type"]
         input_required = input_dataset["required"]
         is_dfr3 = False
+        is_network_dataset = False
 
         # TODO - if input type is not required AND the file size is zero, ignore it
         # This is a workaround because datawolf assumes all datasets are required
         for input_type in input_types:
             if input_type == "incore:dfr3MappingSet":
                 is_dfr3 = True
+            elif input_type in network_dataset_map:
+                is_network_dataset = True
 
         if args.__contains__(input_id):
             dataset_id = args[input_id]
             if len(dataset_id) > 0:
                 if not input_required and os.path.getsize(dataset_id) == 0:
                     print("no input set")
-                elif not is_dfr3:
+                elif is_dfr3:
+                    mapping_set = MappingSet(fragility_service.get_mapping(dataset_id))
+                    analysis.set_input_dataset(input_id, mapping_set)
+                elif is_network_dataset:
+                    analysis.set_input_dataset(input_id, get_network_dataset_object(input_types[0], dataset_id))
+                else:
                     print("found an chained analysis output or local input file")
                     print("input id is: "+input_id)
                     print("data type is "+input_types[0])
-                    analysis.set_input_dataset(input_id, Dataset.from_file(dataset_id, input_types[0]))
-                else:
-                    mapping_set = MappingSet(fragility_service.get_mapping(dataset_id))
-                    analysis.set_input_dataset(input_id, mapping_set)
+                    dataset_local = Dataset.from_file(dataset_id, input_types[0])
+                    if update_dataset_id and "buildingInventory" in input_types[0]:
+                        print("updated buildings for retrofit strategy")
+                        dataset_local.id = "some-id"
+                    analysis.set_input_dataset(input_id, dataset_local)
+                    # analysis.set_input_dataset(input_id, Dataset.from_file(dataset_id, input_types[0]))
+
 
             # if input_id in analysis_input_definitions:
                 #     print("found an chained anlaysis output")
@@ -136,6 +166,66 @@ def set_analysis_inputs(analysis, fragility_service, args):
                 #     else:
             #         mapping_set = MappingSet(fragility_service.get_mapping(dataset_id))
             #         analysis.set_input_dataset(input_id, mapping_set)
+
+def get_network_dataset_object(network_type, dataset_path):
+    # Build the network dataset object
+    network_data_type = network_type
+    network_types = network_dataset_map[network_data_type]
+
+    link_data_type = network_types[0]
+    node_data_type = network_types[1]
+    graph_data_type = network_types[2]
+
+    graph_file_path = find_file(dataset_path, "*.csv")[0]
+    shapefiles = find_file(dataset_path, "*.shp")
+
+    if "Point" in find_shapefile_type(shapefiles[0]):
+        node_file_path = shapefiles[0]
+        link_file_path = shapefiles[1]
+    else:
+        node_file_path = shapefiles[1]
+        link_file_path = shapefiles[0]
+
+    print(node_file_path)
+    print(link_file_path)
+    print(graph_file_path)
+
+    network = NetworkDataset.from_files(
+        node_file_path,
+        link_file_path,
+        graph_file_path,
+        network_data_type,
+        link_data_type,
+        node_data_type,
+        graph_data_type,
+    )
+
+    return network
+        
+def find_file(directory, text):
+    search_path = os.path.join(directory, text)
+
+    # Find matching files
+    matching_files = glob.glob(search_path)
+
+    return matching_files
+
+
+def find_shapefile_type(file_path):
+    try:
+        gdf = gpd.read_file(file_path)
+        if not gdf.empty:
+            # Get the geometry type of the first feature
+            return gdf.geometry.iloc[0].geom_type
+        else:
+            return None
+    except Exception as e:
+        print(f"Error opening shapefile: {e}")
+        return None
+
+def parse_bool(value):
+    bool_map = {"true": True, "false": False}
+    return bool_map.get(value.strip().lower(), False)
 
 
 if __name__ == '__main__':
@@ -159,10 +249,12 @@ if __name__ == '__main__':
     for val in unknown:
         if val.startswith("--"):
             if key is not None:
-                # Hack for fragility keys and result name, which can contain spaces, in which case we don't want to
-                # split the string
+                # Hack for fragility keys, which can contain spaces, in which case we don't want to split the string
                 if len(values.split()) > 1 and key != "fragility_key" and key != "result_name":
                     values = values.split()
+                    # Fix for buyout decision
+                    if key == "residential_archetypes":
+                        values = list(map(int, values))
                 else:
                     values = values.strip()
                 args_dict[key] = values
